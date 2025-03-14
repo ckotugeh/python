565d19cc35249d643b30e0fd2248261e4ffb5453
ckotugeh <ckotugeh@gmail.com>
2025-03-14 20:29:48 +0300

    added tenary operator

--- bmi-app.py
+++ bmi-app.py
@@ -3,15 +3,13 @@
 myName = ""
 height = float
 weight = float
-#prompt the user to entre their name
+#prompt the user to entre their data
 myName = str(input("Your name: " + myName ))
 height = float(input( "Height: " ))
 weight = float(input( "Weight: " ))
 # intialize BMI calculator
 bmi = weight / ( height **2 )     
-if bmi > 25:
-     print(myName + f" Is Obes since the BMI is: {bmi}")
-else:
-     if bmi <= 25:
-          print( myName + f" Is normal since the BMI is: {bmi}")
+# use tenary opperator
+message = myName + f" Is Obes since the BMI is: {bmi}" if bmi <= 25 else myName + f" Is normal since the BMI is: {bmi}"
+print(message)
      
