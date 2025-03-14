31e01e27b95fdd851de7e522a60049b426f08ea6
ckotugeh <ckotugeh@gmail.com>
2025-03-14 19:06:39 +0300

    improved BMI App

--- bmi-app.py
+++ bmi-app.py
@@ -1,14 +1,17 @@
-# This app will calculate BMI of individuals
-# Itialize name, weight and height
-myName = "Charle Otugeh"
-height = 5.56
-weight = 63.5
-
+# Write a program that calculates Body Mass Index of individuals
+# Itialize name, weight and height 
+myName = ""
+height = float
+weight = float
+#prompt the user to entre their name
+myName = str(input("Your name: " + myName ))
+height = float(input( "Height: " ))
+weight = float(input( "Weight: " ))
 # intialize BMI calculator
-bmi = weight / (height **2)
-if bmi >= 30:
-     print(myName + " Is Obes")
+bmi = weight / ( height **2 )     
+if bmi > 25:
+     print( myName + " Is Obes since the BMI is: ", bmi )
 else:
      if bmi <= 25:
-          print(myName + " Is normal")
+          print( myName + " Is normal since the BMI is: ", bmi )
      
